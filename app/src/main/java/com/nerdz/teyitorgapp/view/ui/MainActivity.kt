962fc5124e5e13ca2c0fc1ed261593cb90dc8fad
orcunozyurt <orcunozyurt@hotmail.com>
2019-07-20 18:12:59 +0200

    add fba

--- app/src/main/java/com/nerdz/teyitorgapp/view/ui/MainActivity.kt
+++ app/src/main/java/com/nerdz/teyitorgapp/view/ui/MainActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.nerdz.teyitorgapp.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.main_nav_fragment))
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.main_nav_fragment).navigateUp()
